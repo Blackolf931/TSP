756ff14029a96a1c97cc65fbf7dd65fa2f81f099
DenisKahukin <denis.kagukin@gmail.com>
2021-09-13 13:23:30 +0300

    bugfix

---
+++ TSP/BLL/Services/YoungPeopleSetAdditionalInfoStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class YoungPeopleSetAdditionalInfoStrategy : IStrategy
+    {
+        public bool IsValidStrategy(int age)
+        {
+            if (age > 0 && age <= 18)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string SetInformation()
+        {
+            return new StringBuilder().Append("Your are young people").ToString();
+        }
+    }
+}
