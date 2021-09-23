8d14c23a13d4f1fc6ae87bda665d23dc0ee53a13
Denis Kagykin <52563506+Blackolf931@users.noreply.github.com>
2021-09-23 12:13:20 +0300

    Add Copied Identity Server (#24)
    * Add Copied Identity Server
    
    * Update Config.cs
    
    * Update Program.cs
    
    * Update SecurityHeadersAttribute.cs
    
    * Update Startup.cs
    
    * Update README.md
    
    * Delete README.md

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host...");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
