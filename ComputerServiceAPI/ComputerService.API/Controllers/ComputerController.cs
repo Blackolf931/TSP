b6052174a0ee85a873a61eba034dd93477ef518f
Blaackolf931 <denis.kagukin@gmail.com>
2021-09-23 21:32:39 +0300

    Add Computer Service

---
+++ ComputerServiceAPI/ComputerService.API/Controllers/ComputerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerService.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ComputerController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "GetAll";
+        }
+        [HttpDelete("Delete")]
+        public ActionResult<bool> Delete()
+        {
+            return Ok(true);
+        }
+        [HttpPost("Add")]
+        public ActionResult<string> Add()
+        {
+            return "Computer was been add";
+        }
+        [HttpPut("Update")]
+        public ActionResult<string> Update()
+        {
+            return "Computer was updated";
+        }
+    }
+}
