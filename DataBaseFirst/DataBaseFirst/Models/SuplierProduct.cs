dd820c22541cbd5faa3d3ca996998d7a616e24d8
Denis Kagykin <52563506+Blackolf931@users.noreply.github.com>
2021-10-11 13:09:22 +0300

    Add First DataBase (#32)
    * Add First DataBase
    
    * delete garbage

---
+++ DataBaseFirst/DataBaseFirst/Models/SuplierProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataBaseFirst.Models
+{
+    public partial class SuplierProduct
+    {
+        public int SupplierId { get; set; }
+        public int ProductsId { get; set; }
+
+        public virtual Product Products { get; set; }
+        public virtual Suplier Supplier { get; set; }
+    }
+}
